63103b8d246aee3b7a4a4e77c66ad02c7a22ca3c
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-11-01 00:47:41 +0500

    Envoirnment handling with joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { ConfigModule } from '@nestjs/config';
+import * as joi from 'joi';
 
 @Module({
-  imports: [HelloModule, UserModule, PostModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        APP_NAME: joi.string().default('DefaultAppName'),
+      }),
+    }),
+    HelloModule,
+    UserModule,
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+    const APP_NAME = this.configService.get<string>('APP_NAME')!;
+    return 'Hello World! ' + APP_NAME;
   }
 }
