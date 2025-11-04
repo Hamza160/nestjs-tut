b143b9984bd63f3caea9c2516a9e4382fe8ed0fb
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-11-05 02:17:23 +0500

    Intro to DTOs Done

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // global settings
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips properties that don't have decorators
+      forbidNonWhitelisted: true,
+      transform: true, // transform payload as per their DTO classes
+      disableErrorMessages: true,
+    }),
+  );
   // env settings
   await app.listen(process.env.PORT ?? 3000);
 }
