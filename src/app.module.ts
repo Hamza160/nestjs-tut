702c47300cecb91e4494f55e0819d96387992df2
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-11-11 00:57:38 +0500

    Typeorm with postgresql added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,24 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
 import AppConfig from './config/app.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './posts/entities/post.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [AppConfig],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [Post],
+      synchronize: true, // Dev mode
     }),
     PostsModule,
   ],
